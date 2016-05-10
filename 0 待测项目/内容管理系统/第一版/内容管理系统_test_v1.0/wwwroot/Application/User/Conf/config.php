02243730abd8a91cb13c9747dff67b4651019725
noprom <tyee.noprom@qq.com>
2016-05-10 12:22:50 +0800

    update code

--- 0 待测项目/内容管理系统/第一版/内容管理系统_test_v1.0/wwwroot/Application/User/Conf/config.php
+++ 0 待测项目/内容管理系统/第一版/内容管理系统_test_v1.0/wwwroot/Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', ';*apCbm[!J}w=5D&A.sxNO>hRI,7UG-6S{Ker/|n'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/noc'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:@127.0.0.1:3306/noc'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'cms_'); // 数据表前缀，使用Model方式调用API必须配置此项
--- 0 待测项目/内容管理系统/第一版/内容管理系统_test_v1.0/wwwroot/Runtime/Cache/Home/769e70f2e46f34ceb60619bbda5e4691.php
+++ 0 待测项目/内容管理系统/第一版/内容管理系统_test_v1.0/wwwroot/Runtime/Cache/Home/769e70f2e46f34ceb60619bbda5e4691.php
@@ -3,22 +3,22 @@
 <head>
 	<meta charset="UTF-8">
 <title><?php echo C('WEB_SITE_TITLE');?></title>
-<link href="/wwwroot/Public/static/bootstrap/css/bootstrap.css" rel="stylesheet">
-<link href="/wwwroot/Public/static/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-<link href="/wwwroot/Public/static/bootstrap/css/docs.css" rel="stylesheet">
-<link href="/wwwroot/Public/static/bootstrap/css/onethink.css" rel="stylesheet">
+<link href="/Public/static/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="/Public/static/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+<link href="/Public/static/bootstrap/css/docs.css" rel="stylesheet">
+<link href="/Public/static/bootstrap/css/onethink.css" rel="stylesheet">
 
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
-<script src="/wwwroot/Public/static/bootstrap/js/html5shiv.js"></script>
+<script src="/Public/static/bootstrap/js/html5shiv.js"></script>
 <![endif]-->
 
 <!--[if lt IE 9]>
-<script type="text/javascript" src="/wwwroot/Public/static/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="/Public/static/jquery-1.10.2.min.js"></script>
 <![endif]-->
 <!--[if gte IE 9]><!-->
-<script type="text/javascript" src="/wwwroot/Public/static/jquery-2.0.3.min.js"></script>
-<script type="text/javascript" src="/wwwroot/Public/static/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/Public/static/jquery-2.0.3.min.js"></script>
+<script type="text/javascript" src="/Public/static/bootstrap/js/bootstrap.min.js"></script>
 <!--<![endif]-->
 <!-- 页面header钩子，一般用于加载插件CSS文件和代码 -->
 <?php echo hook('pageHeader');?>
@@ -129,9 +129,9 @@
 <script type="text/javascript">
 (function(){
 	var ThinkPHP = window.Think = {
-		"ROOT"   : "/wwwroot", //当前网站地址
-		"APP"    : "/wwwroot/index.php?s=", //当前项目地址
-		"PUBLIC" : "/wwwroot/Public", //项目公共目录地址
+		"ROOT"   : "", //当前网站地址
+		"APP"    : "/index.php?s=", //当前项目地址
+		"PUBLIC" : "/Public", //项目公共目录地址
 		"DEEP"   : "<?php echo C('URL_PATHINFO_DEPR');?>", //PATHINFO分割符
 		"MODEL"  : ["<?php echo C('URL_MODEL');?>", "<?php echo C('URL_CASE_INSENSITIVE');?>", "<?php echo C('URL_HTML_SUFFIX');?>"],
 		"VAR"    : ["<?php echo C('VAR_MODULE');?>", "<?php echo C('VAR_CONTROLLER');?>", "<?php echo C('VAR_ACTION');?>"]
